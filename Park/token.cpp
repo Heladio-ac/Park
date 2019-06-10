81b22a04ea3f6de5ed596692401986c1c43cbdd2
Heladio <heladio.ac@gmail.com>
2019-06-10 07:18:50 -0500

    Fix display of lexical errors

--- Park/token.cpp
+++ Park/token.cpp
@@ -8,6 +8,8 @@ Token::Token(std::string l, int g) : Symbol(g, true){
 Token::Token() : Symbol() {}
 
 std::string Token::getGrammeme() {
-    grammeme = Symbol::hash(grammeme, lexeme);
+    if (grammeme < 500) {
+        grammeme = Symbol::hash(grammeme, lexeme);
+    }
     return Symbol::getGrammeme();
 }
